c445f8a817afb2720dd4cf8275494434af337dc8
katsiev1997 <katsiev1997@gmail.com>
2023-12-17 00:19:10 +0300

    Create SignIn Page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,31 +1,48 @@
-import React from 'react'
-import { Navbar, Container, Nav, Button, Badge } from 'react-bootstrap'
-import { Outlet } from 'react-router-dom'
-import { Store } from './Store'
-import { LinkContainer } from 'react-router-bootstrap'
-import { ToastContainer } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import {
+  Navbar,
+  Container,
+  Nav,
+  Button,
+  Badge,
+  NavDropdown,
+} from 'react-bootstrap';
+import { Outlet } from 'react-router-dom';
+import { Store } from './Store';
+import { LinkContainer } from 'react-router-bootstrap';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { Link } from 'react-router-dom';
+
 function App() {
   const {
-    state: { mode, cart },
+    state: { mode, cart, userInfo },
     dispatch,
-  } = React.useContext(Store)
+  } = React.useContext(Store);
 
   React.useEffect(() => {
-    document.body.setAttribute('data-bs-theme', mode)
-  }, [mode])
+    document.body.setAttribute('data-bs-theme', mode);
+  }, [mode]);
 
   const switchModeHandler = () => {
-    dispatch({ type: 'SWITCH_MODE' })
-  }
+    dispatch({ type: 'SWITCH_MODE' });
+  };
+
+  const signoutHandler = () => {
+    dispatch({ type: 'USER_SIGNOUT' });
+    localStorage.removeItem('userInfo');
+    localStorage.removeItem('cartItems');
+    localStorage.removeItem('shippingAddress');
+    localStorage.removeItem('paymentInfo');
+    window.location.href = '/signin';
+  };
   return (
-    <div className="d-flex flex-column h-full vh-100">
-      <ToastContainer position="bottom-center" limit={1} />
+    <div className='d-flex flex-column h-full vh-100'>
+      <ToastContainer position='bottom-center' limit={1} />
       <header>
-        <Navbar bg="dark" variant="dark" expand="lg">
+        <Navbar bg='dark' variant='dark' expand='lg'>
           <Container>
-            <LinkContainer to="/">
+            <LinkContainer to='/'>
               <Navbar.Brand>TS Amazona</Navbar.Brand>
             </LinkContainer>
           </Container>
@@ -33,30 +50,42 @@ function App() {
             <Button variant={mode} onClick={switchModeHandler}>
               <i className={mode === 'light' ? 'fa fa-sun' : 'fa fa-moon'}></i>
             </Button>
-            <Link to="/cart" className="nav-link">
+            <Link to='/cart' className='nav-link'>
               Cart
               {cart.cartItems.length > 0 && (
-                <Badge pill bg="danger">
+                <Badge pill bg='danger'>
                   {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
                 </Badge>
               )}
             </Link>
-            <Link to="signin" className="nav-link">
-              Sign In
-            </Link>
+            {userInfo ? (
+              <NavDropdown title={userInfo.name} id='basic-nav-dropdown'>
+                <Link
+                  className='dropdown-item'
+                  to='#signout'
+                  onClick={signoutHandler}
+                >
+                  Sign Out
+                </Link>
+              </NavDropdown>
+            ) : (
+              <Link className='nav-link' to='/signin'>
+                Sign In
+              </Link>
+            )}
           </Nav>
         </Navbar>
       </header>
       <main>
-        <Container className="mt-3">
+        <Container className='mt-3'>
           <Outlet />
         </Container>
       </main>
       <footer>
-        <div className="text-center">All right reserved</div>
+        <div className='text-center'>All right reserved</div>
       </footer>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
